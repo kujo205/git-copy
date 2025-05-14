43b346347960ed386bd6367f7b9524dd4252cddc
kujo205 <kuc8301@gmail.com>
2025-05-14 16:05:22 +0300

    feat: trigger test release

--- src/cli.ts
+++ src/cli.ts
@@ -5,10 +5,9 @@ const version: string = packageJson.version;
 
 const program = new Command();
 
+
 program
 	.version(version)
 	.name("my-command")
 	.option("-d, --debug", "enables verbose logging", false)
 	.parse(process.argv);
-
-// Function code for CLI goes here
