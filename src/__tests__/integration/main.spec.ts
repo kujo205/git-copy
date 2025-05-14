74e54b96beea5347ab85d25c27191747d0aa063a
kujo205 <kuc8301@gmail.com>
2025-05-14 22:59:46 +0300

    chore: run linter

--- src/__tests__/integration/main.spec.ts
+++ src/__tests__/integration/main.spec.ts
@@ -4,11 +4,11 @@ import { resolve } from "path";
 const bin = resolve(__dirname, "./bin.js");
 
 describe("my-command", () => {
-	it("should display the help contents", async () => {
-		const { stdout } = await execa(bin, ["--help"], {
-			env: { TS_NODE_FILES: "true" },
-		});
+  it("should display the help contents", async () => {
+    const { stdout } = await execa(bin, ["--help"], {
+      env: { TS_NODE_FILES: "true" },
+    });
 
-		expect(stdout).toContain("Usage: my-command [options]");
-	});
+    expect(stdout).toContain("Usage: my-command [options]");
+  });
 });
--- src/modules/ArgParser.ts
+++ src/modules/ArgParser.ts
@@ -5,24 +5,24 @@ const packageJson = require("../../package.json");
  * Adapter pattern
  */
 class ArgParser {
-	private program: ReturnType<Command["createCommand"]>;
+  private program: ReturnType<Command["createCommand"]>;
 
-	constructor() {
-		this.program = new Command();
+  constructor() {
+    this.program = new Command();
 
-		const name = packageJson.name;
-		const version: string = packageJson.version;
+    const name = packageJson.name;
+    const version: string = packageJson.version;
 
-		this.program
-			.name(name)
-			.description(packageJson.description)
-			.version(version)
-			.arguments("<source> <destination>")
-			.action((source, destination = ".") => {
-				console.log(`Copying ${source} to ${destination}`);
-			})
-			.parse(process.argv);
-	}
+    this.program
+      .name(name)
+      .description(packageJson.description)
+      .version(version)
+      .arguments("<source> <destination>")
+      .action((source, destination = ".") => {
+        console.log(`Copying ${source} to ${destination}`);
+      })
+      .parse(process.argv);
+  }
 }
 
 export default ArgParser;
